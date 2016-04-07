a513c7e2b0a20dad3d4d2f627b4f3700c5cb03fb
console <stchen@localhost.localdomain>
2016-04-07 22:01:47 +0800

    vector

--- mySTL/include/st_vector.h
+++ mySTL/include/st_vector.h
@@ -26,7 +26,7 @@ class vector {
         typedef 	 T* 			iterator;
         typedef 	 const T*		const_iterator;
 
-    public:
+    private:
         void fill_and_initialize(size_type n, const T& x){
             iterator result = allocate_and_fill(n, x);
             _start = result;
@@ -40,13 +40,51 @@ class vector {
         }
 
     public:
+        bool empty() {return _end == _start;}
         size_type size() {return (_end - _start);}
+
         iterator begin() {return _start; }
+        const_iterator begin() const {return _start;}
+        const_iterator cbegin() const {return _start; }
+
         iterator end() { return _end; }
+        const_iterator end() const {return _end;}
+        const_iterator cend() const { return _end; }
+        
+        iterator rbegin() {return end()-1; }
+        const_iterator rbegin() const { return end()-1;}
+        const_iterator crbegin() const {return cend()-1;}
+
+        iterator rend() {return begin()-1;}
+        const_iterator rend() const { return begin()-1;}
+        const_iterator crend() const {return cbegin()-1;}
+
         vector() : _start(0), _end(0), _capacity(0) {}
 
         vector(size_type n, const value_type& x) { fill_and_initialize(n, x);}
+        ~vector() {
+            if(_capacity != 0) {
+                destroy(_start, _end); 
+                data_allocator.deallocate(_start);
+            }
+        }
 
+        reference operator[](int i) { return *(begin()+i);}
+        const_reference operator[](int i) const { return *(cbegin()+i);}
+
+        reference back() { return *(end()-1); }
+        const_reference back() const { return *(cend()-1); }
+
+        reference front() { return *begin();}
+        const_reference front() const { return *cbegin();}
+
+        reference at(int i) {return *(begin()+i); }
+        const_reference at(int i) const {return *(cbegin()+i); }
+
+        size_type capacity() const {return (_capacity-_start); }
+    
+        pointer data() { return begin();}
+        const_pointer data() const {return cbegin(); }
 
     };
 }
