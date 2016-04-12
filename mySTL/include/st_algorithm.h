d8bd09e571daccde625dd2fb80f44112eb5eb569
console <tim147@outlook.com>
2016-04-12 17:51:21 +0800

    vector --version1

--- mySTL/include/st_algorithm.h
+++ mySTL/include/st_algorithm.h
@@ -22,6 +22,38 @@ namespace tinySTL {
         b = tmp;
     }
 
+    template <class InputIterator1, class InputIterator2>
+        bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2) {
+            while (first1 != last1) {
+                if( *first1 != *first2) return false;
+                ++first1; ++first2;
+            }
+            return true;
+        }
+
+    template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+        bool equal (InputIterator1 first1, InputIterator1 last1,
+                    InputIterator2 first2, BinaryPredicate pred) {
+            while (first1 != last1) {
+                if(!pred(*first1, *first2)) return false;
+                ++first1; ++first2;
+            }
+            return true;
+        }
+
+    template <class InputIterator1, class InputIterator2>
+        bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+                                     InputIterator2 first2, InputIterator2 last2) {
+            while (first1 != last1) {
+                if( first2 == last2 || *first1 > *first2) return false;
+                else if(*first1 < *first2) return true;
+                ++first1; ++first2;
+            }
+            return (first2 != last2);
+        }
+
+
+
     template <class ForwardIterator, class Size, class T>
     ForwardIterator fill_n(ForwardIterator start, Size size, const T& x) {
         for(Size i=0; i<size; ++i)
--- mySTL/include/st_vector.h
+++ mySTL/include/st_vector.h
@@ -12,7 +12,6 @@ class vector {
         T* _start;
         T* _end;
         T* _capacity;
-
         Alloc data_allocator;
 
     public:
@@ -21,26 +20,26 @@ class vector {
         typedef 	 const T* 		const_pointer;
         typedef 	 T& 			reference;
         typedef 	 const T&		const_reference;
-
         typedef 	 size_t 		size_type;
         typedef 	 ptrdiff_t		difference_type;
-
         typedef 	 T* 			iterator;
         typedef 	 const T*		const_iterator;
 
     private:
+        //allocate, fill and initialize the vector
         void fill_and_initialize(size_type n, const T& x){
             iterator result = allocate_and_fill(n, x);
             _start = result;
             _end = result + n;
             _capacity = _start + n;
         }
+        //allocate and fill the vector
         iterator allocate_and_fill(size_type n, const T& x) {
             iterator result = static_cast<iterator>(data_allocator.allocate(n));
             _uninitialed_fill_n(result, n, x);
             return result;
         }
-
+        //reallocate the vector and copy the contents from iterator from begin and end
         template< class InputIterator>
         InputIterator reallocate_and_copy(InputIterator begin, InputIterator end) {
             size_type n = end-begin;
@@ -48,7 +47,7 @@ class vector {
             uninitialed_copy(begin, end, result);
             return result;
         }
-
+        //allocate the vector and copy the contents from iterator from begin and end
         template <class InputIterator>
         InputIterator allocate_and_copy(InputIterator begin, InputIterator end) {
             size_type n = end - begin;
@@ -56,7 +55,7 @@ class vector {
             uninitialed_copy(begin, end, result);
             return result;
         }
-
+        //prepared for insert function and push_back function, insert element before position
         template <class InputIterator>
         iterator insert_aux(const_iterator position, InputIterator first, InputIterator last) {
             size_type n = last - first;
@@ -87,7 +86,7 @@ class vector {
                 return respos;
             }
         }
-        
+        //prepared for insert function and push_back function, insert element before position
         iterator insert_aux(const_iterator position, const value_type& val) {
             iterator pos = const_cast<iterator>(position);
             if(_end < _capacity) {
@@ -119,69 +118,66 @@ class vector {
             }         
         }
     public:
+        //Returns whether the vector is empty 
         bool empty() {return _end == _start;}
+        //Returns the number of elements in the vector.
         size_type size() const {return (_end - _start);}
-
+        //Returns an iterator pointing to the first element in the vector.
         iterator begin() {return _start; }
         const_iterator begin() const {return _start;}
         const_iterator cbegin() const {return _start; }
-
+        //Returns an iterator referring to the past-the-end element in the vector container.
         iterator end() { return _end; }
         const_iterator end() const {return _end;}
         const_iterator cend() const { return _end; }
-        
+        //Return reverse iterator to reverse beginning 
         iterator rbegin() {return end()-1; }
         const_iterator rbegin() const { return end()-1;}
         const_iterator crbegin() const {return cend()-1;}
-
+        //Return reverse iterator to reverse end
         iterator rend() {return begin()-1;}
         const_iterator rend() const { return begin()-1;}
         const_iterator crend() const {return cbegin()-1;}
-
-       vector() : _start(0), _end(0), _capacity(0) {}
-
-
+        //Constructs a vector, initializing its contents depending on the constructor version used
+        vector() : _start(0), _end(0), _capacity(0) {}
         vector(size_type n, const value_type& x) { fill_and_initialize(n, x);}
-        
         vector(vector<T>& x) {
             _start = allocate_and_copy<iterator>(x.begin(), x.end());
             _end = _start + x.size();
             _capacity = _start + x.size();
         }
-
         vector(const vector<value_type>& x) {
             _start = const_cast<iterator>(allocate_and_copy<const_iterator>(x.begin(), x.end()));
             _end = _start + x.size();
             _capacity = _start + x.size();
         }
-
+        //Destroys the container object
         ~vector() {
             if(_capacity != 0) {
                 destroy(_start, _end); 
                 data_allocator.deallocate(_start);
             }
         }
-
+        //Returns a reference to the element at position n in the vector container.
         reference operator[](int i) { return *(begin()+i);}
         const_reference operator[](int i) const { return *(cbegin()+i);}
-
+        //Returns a reference to the last element in the vector.
         reference back() { return *(end()-1); }
         const_reference back() const { return *(cend()-1); }
-
+        //Returns a reference to the first element in the vector
         reference front() { return *begin();}
         const_reference front() const { return *cbegin();}
-
+        //Returns a reference to the element at position n in the vector
         reference at(int i) {return *(begin()+i); }
         const_reference at(int i) const {return *(cbegin()+i); }
-
+        //Return size of allocated storage capacity
         size_type capacity() const {return (_capacity-_start); }
-    
+        //Returns a direct pointer to the memory array used internally by the vector to store its owned elements.
         pointer data() { return begin();}
         const_pointer data() const {return cbegin(); }
-
+        //Returns a copy of the allocator object associated with the vector
         Alloc get_allocator() { return data_allocator;}
-
-
+        //Assigns new contents to the container, replacing its current contents, and modifying its size accordingly.
         vector& operator=(const vector<T>& x) {
             //const int size =x.size();
             int newsize = x.size();
@@ -210,7 +206,7 @@ class vector {
 
             return *this;
         }
-
+        //Assigns new contents to the container, replacing its current contents, and modifying its size accordingly.
         vector& operator=(vector<T>& x) { //move
             _start = reallocate_and_copy(x.begin(), x.end());
             _end = _start + x.size();
@@ -224,17 +220,17 @@ class vector {
         iterator insert(const_iterator position, const value_type& val) {
             return insert_aux(position, val);
         }
-        
+        //The vector is extended by inserting new elements before the element at the specified position
         iterator insert(const_iterator position, size_type n, const value_type& val) {
            vector<value_type> tmp(n, val);
            return insert_aux<iterator>(position, tmp.begin(), tmp.end());
         }
-
+        //The vector is extended by inserting new elements before the element at the specified position
         template <class InputIterator>
         iterator insert(const_iterator position, InputIterator first, InputIterator last) {
             return insert_aux<InputIterator>(position, first, last); 
         }
-
+        //The vector is extended by inserting new elements before the element at the specified position
         iterator insert(const_iterator position, value_type& val) {
             return insert_aux(position, val); 
         }
@@ -248,14 +244,13 @@ class vector {
             else 
                 insert(end(), val);
         }
-        
         // erase element at the end
         void pop_back() {
             assert(_end != _start);
             --_end;
             destroy(_end);
         }
-
+        //ves from the vector either a single element (position) 
         iterator erase(iterator position) {
             if(position+1 != _end)
                 copy(position+1, end(), position);
@@ -263,7 +258,7 @@ class vector {
             destroy(_end);
             return position;
         }
-
+        //Removes from the vector a range of elements ([first,last)).
         iterator erase(iterator first, iterator last) {
             size_type n = last - first;
             if(last != end() )
@@ -272,18 +267,16 @@ class vector {
             _end = _end - n;
             return first;
         }
-
+        //Removes all elements from the vector (which are destroyed), leaving the container with a size of 0.
         void clear(){
             erase(_start, _end);
         }
-
         //Exchanges the content of the container by the content of x
         void swap(vector<value_type>& x) {
             tinySTL::swap<iterator>(_start, x._start);
             tinySTL::swap<iterator>(_end, x._end);
             tinySTL::swap<iterator>(_capacity, x._capacity);
         }
-
         //vector capacity be at least enough to contain n elements
         void reserve (size_type n) {
             if(capacity() < n) {
@@ -301,16 +294,49 @@ class vector {
                _capacity = _start + n; 
             }
         }
-
+        //Assigns new contents to the vector, replacing its current contents
         template <class InputIterator>
-        void assign (InputIterator first, InputIterator last) {}
+        void assign (InputIterator first, InputIterator last) {
+            size_type n = last - first;
+            if(n <= capacity() ) {
+                destroy(_start, _end);
+                _end = uninitialed_fill(first, last, _start);
 
-        void assign (size_type n, const value_type& val) {}
+            } else {
+                iterator result = data_allocator.allocate(n);
+                iterator newstart = result;
+                result = uninitialed_fill(first,last, result);
 
+                destroy(_start, _end);
+                data_allocator.deallocate(_start);
+
+                _start = newstart;
+                _end = result;
+                _capacity = _start + n;
+            }
+        }
+        //Assigns new contents to the vector, replacing its current contents
+        void assign (size_type n, const value_type& val) {
+            if(n <= capacity()) {
+                destroy(_start, _end);
+                _end = _uninitialed_fill_n(_start, n, val);
+            } else {
+                iterator result = data_allocator.allocate(n);
+                iterator newstart = result;
+                result = _uninitialed_fill_n(result, n, val);
+
+                destroy(_start, _end);
+                data_allocator.deallocate(_start);
+                _start = newstart;
+                _end = result;
+                _capacity = _start + n;
+            }
+        }
+        //Resizes the container so that it contains n elements
         void resize (size_type n) {
             resize(n, value_type());
         }
-
+        //Resizes the container so that it contains n elements
         void resize (size_type n, const value_type& val) {
             if(size() < n) {
                 if(capacity() >= n) { 
@@ -335,20 +361,46 @@ class vector {
                 _end = _start + n;
             }
         }
-
         // reduce its capacity to fit its size
         void shrink_to_fit() {
             reserve(size());
         }
-
-        size_type max_size() const {}
-
-
-
-
-
-
+        //Returns the maximum number of elements that the vector can hold.
+        size_type max_size() const {
+            return size_type(-1) / sizeof(value_type); 
+        }
     };
+    
+    //comparison operation between the vector containers x and y
+    template <class T, class Alloc>
+    inline bool operator==(const vector<T,Alloc>& x, const vector<T, Alloc>& y) {
+       return (x.size() == y.size() && equal(x.begin(), x.end(), y.begin()));
+    }
+    template <class T, class Alloc>
+    inline bool operator!=(const vector<T,Alloc>& x, const vector<T, Alloc>& y) {
+       return !(x==y); 
+    }
+    template <class T, class Alloc>
+    inline bool operator<(const vector<T,Alloc>& x, const vector<T, Alloc>& y) {
+       return lexicographical_compare(x.begin(), x.end(), y.begin(), y.end()); 
+    }
+    template <class T, class Alloc>
+    inline bool operator<=(const vector<T,Alloc>& x, const vector<T, Alloc>& y) {
+        return (x==y || x<y);
+    }
+    template <class T, class Alloc>
+    inline bool operator>(const vector<T,Alloc>& x, const vector<T, Alloc>& y) {
+        return lexicographical_compare(y.begin(), y.end(), x.begin(), x.end());
+    }
+    template <class T, class Alloc>
+    inline bool operator>=(const vector<T,Alloc>& x, const vector<T, Alloc>& y) {
+        return (x==y || x>y);
+    }
+    //The contents of container x are exchanged with those of y
+    template <class T, class Alloc>
+        inline void swap (vector<T, Alloc>& x, vector<T, Alloc>& y) {
+            x.swap(y);
+        }
 }
-#endif // ST_tECTOR_H
+#endif // ST_VECTOR_H
 
