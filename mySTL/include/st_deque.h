8b702bc0be3bdf2c8b8793fbb726ae4c1f4e02a6
console <tim147@outlook.com>
2016-05-22 16:49:37 +0800

    deque-v2

--- mySTL/include/st_deque.h
+++ mySTL/include/st_deque.h
@@ -15,8 +15,8 @@ template <class T, class Ptr, class Ref>
 class deque_iterator {
     protected:
         typedef deque_iterator<T, T*, T&> iterator;
-        typedef deque_iterator<const T, const T*, const T&> const_iterator;
-
+        typedef const deque_iterator<T, T*, T&> const_iterator;
+        //data buffer size
         static size_t buffer_size () { return _deque_buf_size (sizeof(T)); }
 
         typedef random_access_iterator_tag  iterator_category;
@@ -36,17 +36,18 @@ class deque_iterator {
         map_pointer node;
 
     public:
-
+        //update a new node
         void set_node (map_pointer new_node) {
             node = new_node;
             first = *new_node;
             last = first + difference_type (buffer_size());
         }
-
+        //overload operattions for iterator
         reference operator* () const { return *cur; }
+
         pointer operator-> () const { return cur; }
 
-        difference_type operator- (self x) {
+        difference_type operator- (const iterator& x) const {
             return difference_type (buffer_size() ) * (node - x.node - 1)
                     + (cur - first) + (x.last - x.cur);
         }
@@ -107,13 +108,14 @@ class deque_iterator {
         self operator- (difference_type size) {
             self tmp = *this;
             tmp -= size;
-               return tmp;
+            return tmp;
         }
+
 
         reference operator[] (difference_type n) {
             return *(*this + n);
         }
-           
+        //overload relationships of iterator   
         bool operator== (const self& x) const { return cur==x.cur;}
         bool operator!= (const self& x) const { return cur != x.cur; }
         bool operator< (const self& x) const { return (x.node == node ? cur < x.cur : node < x.node); }
@@ -122,6 +124,7 @@ class deque_iterator {
         bool operator>= ( const self& x) const { return *this > x || *this == x; }
 };
 
+//default use simpleAlloc
 template <class T, class Alloc = SimpleAlloc>
     class deque {
         private:
@@ -134,9 +137,10 @@ template <class T, class Alloc = SimpleAlloc>
             typedef ptrdiff_t    difference_type;
         public:
             typedef deque_iterator<value_type, pointer, reference> iterator;
-            typedef deque_iterator<const T, const T*, const T&> const_iterator;
+            typedef const deque_iterator<T, T*, T&> const_iterator;
         
         protected:
+            //map pointer
             typedef pointer* map_pointer;
 
         protected:
@@ -145,13 +149,13 @@ template <class T, class Alloc = SimpleAlloc>
 
             map_pointer map;
             size_type map_size;
-            
+            //two different allocator
             simple_alloc<value_type, Alloc>   data_allocator;
             simple_alloc<pointer, Alloc>      map_allocator;
         private:
-
+            //return buffer size
             size_type buf_size () const { return _deque_buf_size (sizeof (T)); }
-
+            //allocate a new node
             pointer allocate_node () {
                 return data_allocator.allocate(buf_size());
             }
@@ -159,34 +163,28 @@ template <class T, class Alloc = SimpleAlloc>
             void deallocate_node (pointer buff_ptr) {
                 data_allocator.deallocate (buff_ptr);
             }
-
+            //create a new node and map
             void create_map_and_node (const size_type& num_elems) {
                 size_type node_num = num_elems / buf_size() + 1;
-                
                 map_size = max((size_type)8, node_num+2);
                 map = map_allocator.allocate(map_size);
-
                 map_pointer nstart = map + (map_size - node_num)/2;
                 map_pointer nend = nstart + node_num - 1;
-
                 for (map_pointer cur = nstart; cur != nend + 1; ++cur)
                     *cur = allocate_node();
-
                 start.set_node (nstart);
                 finish.set_node (nend);
-
                 start.cur = start.first;
                 finish.cur = finish.first + num_elems % buf_size();
             }
             // iniilize n nodes
             void fill_initiallize (size_type n, const value_type& value) {
                 create_map_and_node (n);
-
                 for (map_pointer tcur = start.node; tcur != finish.node; ++tcur) 
                    uninitialled_fill (*tcur, *tcur + buf_size(), value);
                 uninitialled_fill (finish.first, finish.cur, value);
             }
-
+            //reallocate a map if the map need to expand
             void reallocate_map (size_type node_to_add, bool add_front) {
                 size_type old_node_num = finish.node - start.node + 1;
                 size_type new_node_num = old_node_num + node_to_add;
@@ -211,16 +209,17 @@ template <class T, class Alloc = SimpleAlloc>
                 start.set_node(new_start);
                 finish.set_node (new_start + old_node_num - 1);
             }
-
+            //check if the map is empty at the end
             void reserve_map_at_back (size_type node_to_add = 1) {
                 if (node_to_add + 1 > map_size - (finish.node - start.node + 1) )
                     reallocate_map (node_to_add, false);
             }
-
+            //check if the map have enough space at the front
             void reserve_map_at_front (size_type node_to_add = 1) {
                 if (node_to_add > start.node - map) 
                     reallocate_map (node_to_add, true);
             }
+            //push_back element in the end 
              void push_back_aux (const value_type& value) {
                  reserve_map_at_back ();
                  *(finish.node + 1) = data_allocator.allocate ();
@@ -228,7 +227,7 @@ template <class T, class Alloc = SimpleAlloc>
                  finish.set_node (finish.node + 1);
                  finish.cur = finish.first;
              }
-
+            //push the element at the front
              void push_front_aux ( const value_type& value) {
                 reserve_map_at_front ();
                 *(start.node - 1) = data_allocator.allocate ();
@@ -236,24 +235,23 @@ template <class T, class Alloc = SimpleAlloc>
                 start.cur = start.last - 1;
                 construct (start.cur, value);
              }
-             
+             //remove the element at the back
              void pop_back_aux () {
                 deallocate_node (finish.first);
                 finish.set_node (finish.node -1 );
                 finish.cur = finish.last - 1;
                 destroy (finish.cur);
              }
-
+            //remove the element at the front
              void pop_front_aux () {
                 destroy (start.cur);
                 deallocate_node (start.first);
                 start.set_node (start.node + 1);
                 start.cur = start.first;
              }
-
+            //insert the element while the element is not at the front or back
              iterator insert_aux (iterator pos, const value_type& val) {
                 difference_type index = pos - start;
-
                 if (index < (size()>>1) ) {
                     push_front(front());
                     iterator old_front = start;
@@ -274,24 +272,23 @@ template <class T, class Alloc = SimpleAlloc>
 
              }
 
-
-
         public:
+            //Returns an iterator pointing to the first element in the deque container
             iterator begin () { return start; }
             const_iterator begin () const { return start; }
             const_iterator cbegin () const { return start; }
-
+            //Returns an iterator referring to the past-the-end element in the deque container.
             iterator end () { return finish; }
             const_iterator end () const { return finish; }
             const_iterator cend () const { return finish; }
-
+            //Returns a reference to the element at position n in the deque container
             reference operator[] (size_type n) {
                 return start[difference_type(n)];
             }
-
+            //Returns a reference to the first element in the deque container
             reference front () { return *start; }
             const_reference front () const { return *start; }
-
+            //Returns a reference to the last element in the container
             reference back () { 
                 iterator tmp = finish;
                 --tmp;
@@ -302,28 +299,28 @@ template <class T, class Alloc = SimpleAlloc>
                 --tmp;
                 return *tmp;
             }
-
-            size_type size () { return (size_type)(finish - start); }
-
+            //Returns the number of elements in the deque container.
+            size_type size () const { return (size_type)(end() - begin()); }
+            //Returns the maximum number of elements that the deque container can hold
             size_type max_size () const { return size_type(-1); }
-
+            //check if the deque is empty
             bool empty () const { return start == finish; }
-
+            //deque constructor
             deque (size_type n, const value_type& value)
                 : start(), finish(), map(0), map_size(0) {
                     fill_initiallize (n, value);
                 }
-
             explicit deque (size_type n) 
                 : start(), finish(), map(0), map_size(0) {
                     fill_initiallize (n, value_type());
                 }
+            //Destroys the container object
              ~deque () {
                 clear();
                 data_allocator.deallocate (*start.node);
                 map_allocator.deallocate (map);
              }
-
+            //add a element at the back
             void push_back (const value_type& value) {
                 if (finish.cur != finish.last - 1) {
                     construct(finish.cur, value);
@@ -331,7 +328,7 @@ template <class T, class Alloc = SimpleAlloc>
                 } else 
                     push_back_aux (value);
             }
-
+            //add element at the front
             void push_front (const value_type& value) {
                 if (start.cur != start.first) {
                     construct (start.cur, value);
@@ -339,7 +336,7 @@ template <class T, class Alloc = SimpleAlloc>
                 } else 
                     push_front_aux (value);
             }
-
+            //remove element at the back
             void pop_back () {
                 if (finish.cur != finish.first) {
                     destroy (finish.cur);
@@ -347,7 +344,7 @@ template <class T, class Alloc = SimpleAlloc>
                 } else 
                     pop_back_aux ();
             }
-
+            //remove element at the front
             void pop_front () {
                 if (start.cur != start.last - 1) {
                     destroy (start.cur);
@@ -357,12 +354,10 @@ template <class T, class Alloc = SimpleAlloc>
             }
             //come back to the initiallize state
             void clear () {
-
                 for (map_pointer node = start.node + 1; node < finish.node; ++node) {
                     destroy (*node, *node + buf_size());
                     data_allocator.deallocate (*node);
                 }
-
                 // reserve a buffer
                 if ( start.node != finish.node) {
                     destroy (start.cur, start.last);
@@ -388,7 +383,7 @@ template <class T, class Alloc = SimpleAlloc>
                 }
                 return start + index;
             }
-
+            //erase several element
             iterator erase (iterator first, iterator last) {
                 if (first == start && last == finish) {
                     clear();
@@ -415,7 +410,7 @@ template <class T, class Alloc = SimpleAlloc>
                     return  start + to_start;
                 }
             }
-            
+            //insert element
             iterator insert (iterator pos, const value_type& val) {
                 if (pos == start) {
                     push_front (val);
@@ -428,7 +423,32 @@ template <class T, class Alloc = SimpleAlloc>
             }
             
     };
+    //Relational operators for deque
+    template <class T>
+        inline bool operator== (const deque<T>& lhs, const deque<T>& rhs) {
+            return lhs.size() == rhs.size() && equal (lhs.begin(), lhs.end(), rhs.begin());
+        }
+    template <class T>
+        inline bool operator!= (const deque<T>& lhs, const deque<T>& rhs) {
+            return !(lhs == rhs);
+        }
+    template <class T>
+        inline bool operator< (const deque<T>& lhs, const deque<T>& rhs) {
+            return lexicographical_compare (lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+        }
+    template <class T>
+        inline bool operator<= (const deque<T>& lhs, const deque<T>& rhs) {
+            return lhs == rhs || lhs < rhs;
+        }
 
+    template <class T>
+        inline bool operator> (const deque<T>& lhs, const deque<T>& rhs) {
+            return !(lhs <= rhs);
+        }
+    template <class T>
+        inline bool operator>= (const deque<T>& lhs, const deque<T>& rhs) {
+            return lhs == rhs || lhs > rhs;
+        }
 }
 
 #endif
--- mySTL/test/deque_test.cpp
+++ mySTL/test/deque_test.cpp
@@ -12,8 +12,20 @@ int main () {
     deq.pop_front();
     deq.pop_back();
     int n = deq.size();
+    std::cout<<n<<std::endl;
     for (int i=0; i< n; ++i) 
         std::cout<<deq[i]<<std::endl;
+
+    tinySTL::deque<int> foo (3,100);   // three ints with a value of 100
+    tinySTL::deque<int> bar (2,200);   // two ints with a value of 200
+
+    if (foo==bar) std::cout << "foo and bar are equal\n";
+    if (foo!=bar) std::cout << "foo and bar are not equal\n";
+    if (foo< bar) std::cout << "foo is less than bar\n";
+    if (foo> bar) std::cout << "foo is greater than bar\n";
+    if (foo<=bar) std::cout << "foo is less than or equal to bar\n";
+    if (foo>=bar) std::cout << "foo is greater than or equal to bar\n";
+
 
 
     return 0;
